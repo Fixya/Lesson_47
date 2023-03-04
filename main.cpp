a62ccdfc5d0a013d3be85c5301bcb16754abd661
Fixya <114008002+Fixya@users.noreply.github.com>
2023-03-04 19:50:23 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+class Computer
+{
+private:
+	string processor;
+	size_t ram;
+	size_t hdd;
+public:
+	Computer()
+	{
+		processor = "";
+		ram = 0;
+		hdd = 0;
+	}
+	Computer(string fprocessor, size_t fram, size_t fhdd) 
+	{
+		processor = fprocessor;
+		ram = fram;
+		hdd = fhdd;
+	}
+	void print()
+	{
+		cout << "Processor: " << processor << endl;
+		cout << "Ram: " << ram << "Gb" << endl;
+		cout << "HDD: " << hdd << "Gd" << endl;
+	}
+};
+
+int main()
+{
+	vector<Computer> v1;
+	string arr_name[] = { "ghg", "hgh", "pnp", "npn" };
+	srand(time(0));
+	for (int i = 0; i < 5; i++)
+	{
+		int a = rand() % 4;
+		int b = rand() % 10;
+		int c = rand() % 512;
+		Computer ci(arr_name[a], b, c);
+		v1.push_back(ci);
+	}
+	for (int i = 0; i < v1.size(); i++)
+	{
+		v1.at(i).print();
+	}
+	return 0;
+}
---
+++ main1.cpp
@@ -0,0 +1,46 @@
+﻿#include <SFML/Graphics.hpp>
+#include <vector>
+
+using namespace sf;
+using namespace std;
+class Art
+{
+private:
+	Texture texture;
+	Sprite sprite;
+public:
+	Art(Vector2f pos) {
+		texture.loadFromFile("box.jpg");
+		sprite.setTexture(texture);
+		sprite.setPosition(pos);
+	}
+	Sprite getSprite() { return sprite; }
+};
+
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "SFML Works!");
+
+	vector<Art*> v1;
+	for (int i = 0; i < 5; i++)
+	{
+		Art* c1 = new Art (Vector2f( 100 * i, 50.f ));
+		v1.push_back(c1);
+	}
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		for (int i = 0; i < 5; i++)
+		{
+			window.draw(v1.at(i)->getSprite());
+		}
+		window.display();
+	}
+	return 0;
+}
